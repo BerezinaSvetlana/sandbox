a0ef01ef445689e85f9f64bf0691304787753dbc
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-16 21:10:44 +0400

    Добавлены поля класса (имя, рост, вес и день рождения)

--- src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/Person.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/Person.java
@@ -1,4 +1,13 @@
 package ru.ssau.tk.abrosimovamargo.sandbox;
 
+import java.util.Date;
+
 public class Person {
+
+    public String name;
+    public double height;
+    public double weight;
+    public Date birtday;
+
+
 }
