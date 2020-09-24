5536e449a28c9a76c35fcdd1b95edb65f8b707b0
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-09-24 11:16:18 +0300

    создан тест result

--- src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/functions/CompositeFunctionTest.java
+++ src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/functions/CompositeFunctionTest.java
@@ -17,5 +17,7 @@ public class CompositeFunctionTest {
         assertEquals(sqrsqr.apply(2), 16.0 );
         MathFunction identitysqr = new CompositeFunction(identityFunction, sqtFunction);
         assertEquals(identitysqr.apply(6), 36.0);
+        double result = sqtFunction.andThen(asinFunction).apply(0.0);
+        assertEquals(result, 0.0);
     }
 }
