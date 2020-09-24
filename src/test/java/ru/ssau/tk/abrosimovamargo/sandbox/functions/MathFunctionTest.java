95bd44e0065267fd58fc3385f3848a0664482c70
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-09-24 11:01:46 +0300

    создан метод CompositeFunction andThen(MathFunction afterFunction) с реализацией по умолчанию

---
+++ src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/functions/MathFunctionTest.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.abrosimovamargo.sandbox.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MathFunctionTest {
+
+    @Test
+    public void testAndThen() {
+    }
+}
