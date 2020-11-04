0eeac06123fa612d2f50be8d72351e7b8f51f79c
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-11-04 18:03:58 +0400

    Создала класс  LinkedListTabulatedFunction

---
+++ src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/functions/LinkedListTabulatedFunction.java
@@ -0,0 +1,63 @@
+package ru.ssau.tk.abrosimovamargo.sandbox.functions;
+
+public class LinkedListTabulatedFunction extends AbstractTabulatedFunction{
+    @Override
+    int floorIndexOfX(double x) {
+        return 0;
+    }
+
+    @Override
+    double extrapolateLeft(double x) {
+        return 0;
+    }
+
+    @Override
+    double extrapolateRight(double x) {
+        return 0;
+    }
+
+    @Override
+    double interpolate(double x, int floorIndex) {
+        return 0;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public double getX(int index) {
+        return 0;
+    }
+
+    @Override
+    public double getY(int index) {
+        return 0;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        return 0;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        return 0;
+    }
+
+    @Override
+    public double leftBound() {
+        return 0;
+    }
+
+    @Override
+    public double rightBound() {
+        return 0;
+    }
+}
