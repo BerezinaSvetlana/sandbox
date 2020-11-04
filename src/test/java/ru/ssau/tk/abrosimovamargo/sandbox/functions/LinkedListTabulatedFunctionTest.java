639c90312670a7f67ac44f20b91430443cc4374f
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-11-04 22:45:38 +0400

    +1 работающий тестик

--- src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/functions/LinkedListTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/functions/LinkedListTabulatedFunctionTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class LinkedListTabulatedFunctionTest {
+    final double DELTA = 0.0001;
         private final double[] xValues = new double[]{1, 2, 3, 4, 5};
         private final double[] yValues = new double[]{11, 22, 33, 44, 55};
         private final MathFunction testFunction = new SqrFunction();
@@ -17,6 +18,16 @@ public class LinkedListTabulatedFunctionTest {
         private LinkedListTabulatedFunction getListOfMathFunction() {
             return new LinkedListTabulatedFunction(testFunction, 5, 15, 30);
         }
+
+    @Test
+    public void testGetNode() {
+        assertEquals(getListOfArray().getX(0), 1, DELTA);
+        assertEquals(getListOfArray().getX(1), 2, DELTA);
+        assertEquals(getListOfArray().getX(2), 3, DELTA);
+        assertEquals(getListOfArray().getX(3), 4, DELTA);
+        assertEquals(getListOfArray().getX(4), 5, DELTA);
+
+    }
 
     @Test
     public void testFloorIndexOfX() {
