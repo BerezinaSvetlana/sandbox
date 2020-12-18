ae1718f13baef3c96a9c0a71c50a7eaf70597f49
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-18 23:40:07 +0300

    ЛР3 Задание Y3.3

--- src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/io/FunctionsIO.java
@@ -1,9 +1,23 @@
 package ru.ssau.tk.abrosimovamargo.sandbox.io;
 
 
+import ru.ssau.tk.abrosimovamargo.sandbox.functions.Point;
+import ru.ssau.tk.abrosimovamargo.sandbox.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point a : function) {
+            printWriter.printf("%f %f\n", a.x, a.y);
+        }
+        printWriter.flush();
 
+    }
 }
