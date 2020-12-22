ef45daf13efbe83130f5dcf77b7568e53220a5a5
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-22 19:50:03 +0300

    ЛР3.2 Исправлены замечания

--- src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/io/FunctionsIO.java
@@ -3,9 +3,15 @@ package ru.ssau.tk.abrosimovamargo.sandbox.io;
 
 import ru.ssau.tk.abrosimovamargo.sandbox.functions.Point;
 import ru.ssau.tk.abrosimovamargo.sandbox.functions.TabulatedFunction;
+import ru.ssau.tk.abrosimovamargo.sandbox.functions.factory.TabulatedFunctionFactory;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -20,4 +26,23 @@ public final class FunctionsIO {
         printWriter.flush();
 
     }
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException, IOException {
+        int count = Integer.parseInt(reader.readLine());
+
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            String line = reader.readLine();
+            String[] splitLine = line.split(" ");
+            try {
+                xValues[i] = numberFormat.parse(splitLine[0]).doubleValue();
+                yValues[i] = numberFormat.parse(splitLine[1]).doubleValue();
+            } catch (ParseException eParse) {
+                throw new IOException(eParse);
+            }
+        }
+        return factory.create(xValues, yValues);
+    }
+
 }
