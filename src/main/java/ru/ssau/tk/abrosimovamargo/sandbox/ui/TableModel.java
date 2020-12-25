a114ab7ed8f55c1219433e1e836790b49fffcfe1
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-25 19:46:49 +0400

    ура, перестала идти красная волнистая полоса моей жизни

--- src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/ui/TableModel.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/sandbox/ui/TableModel.java
@@ -1,49 +1,53 @@
 package ru.ssau.tk.abrosimovamargo.sandbox.ui;
 
-import ru.ssau.tk.abrosimovamargo.sandbox.functions.TabulatedFunction;
-
 import javax.swing.table.AbstractTableModel;
+import java.util.List;
 
-public class TableModelMainWindow extends AbstractTableModel {
-    private static final int INDEX_COLUMN_NUMBER = 0;
-    private static final int X_COLUMN = 1;
-    private static final int Y_COLUMN = 2;
-    private TabulatedFunction function;
+public class TableModel extends AbstractTableModel {
+    private static final int X_COLUMN = 0;
+    private static final int Y_COLUMN = 1;
+    private List<Double> xValues;
+    private List<Double> yValues;
 
-    public TableModelMainWindow() {
+    public TableModel(List<Double> xValues, List<Double> yValues) {
+        this.xValues = xValues;
+        this.yValues = yValues;
     }
 
     @Override
     public int getRowCount() {
-        return (function == null) ? 0 : function.getCount();
+        return xValues.size();
     }
 
     @Override
     public int getColumnCount() {
-        return 3;
+        return 2;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         switch (columnIndex) {
-            case INDEX_COLUMN_NUMBER:
-                return rowIndex;
             case X_COLUMN:
-                return function.getX(rowIndex);
+                return xValues.get(rowIndex);
             case Y_COLUMN:
-                return function.getY(rowIndex);
+                return yValues.get(rowIndex);
         }
         throw new UnsupportedOperationException();
     }
 
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) throws NumberFormatException {
-        if (columnIndex == Y_COLUMN) {
+        if (columnIndex == X_COLUMN) {
             try {
-                function.setY(rowIndex, Double.parseDouble(aValue.toString()));
+                xValues.set(rowIndex, Double.valueOf(aValue.toString()));
             } catch (Exception e) {
-
-                function.setY(rowIndex, 0.0);
+                xValues.set(rowIndex, 0.0);
+            }
+        } else if (columnIndex == Y_COLUMN) {
+            try {
+                yValues.set(rowIndex, Double.valueOf(aValue.toString()));
+            } catch (Exception e) {
+                yValues.set(rowIndex, 0.0);
             }
         }
     }
@@ -51,9 +55,7 @@ public class TableModelMainWindow extends AbstractTableModel {
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         switch (columnIndex) {
-            case INDEX_COLUMN_NUMBER:
             case X_COLUMN:
-                return false;
             case Y_COLUMN:
                 return true;
         }
@@ -63,21 +65,11 @@ public class TableModelMainWindow extends AbstractTableModel {
     @Override
     public String getColumnName(int column) {
         switch (column) {
-            case INDEX_COLUMN_NUMBER:
-                return "Индекс";
             case X_COLUMN:
                 return "X";
             case Y_COLUMN:
                 return "Y";
         }
         return super.getColumnName(column);
-    }
-
-    public TabulatedFunction getFunction() {
-        return function;
-    }
-
-    public void setFunction(TabulatedFunction function) {
-        this.function = function;
     }
 }
