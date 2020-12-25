d93e8a4fdefce7ecf637d445c04f6ac5ead8e202
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-25 22:11:31 +0400

    тест

--- src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/operations/RightSteppingDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/abrosimovamargo/sandbox/operations/RightSteppingDifferentialOperatorTest.java
@@ -9,12 +9,12 @@ public class RightSteppingDifferentialOperatorTest {
 
 
     private static final double STEP = 0.01;
-
+    final double DElTA = 0.0001;
 
     @Test
     public void testDerive() {
         SteppingDifferentialOperator differentialOperator = new RightSteppingDifferentialOperator(STEP);
-        assertEquals(differentialOperator.derive(new SqrFunction()).apply(3), 6.0099, 0.0001);
-        assertEquals(differentialOperator.derive(new SqrFunction()).apply(4), 8.009, 0.0001);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(3), 6.01, DElTA);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(4), 8.01, DElTA);
     }
 }
